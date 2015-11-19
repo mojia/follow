ffd64b7ddb29f35fd9b153c2bf4c2c86d39d7124
wangxin <wangxin@robertdeMacBook-Pro.local>
2015-11-19 23:37:13 +0800

    start

---
+++ bible/src/main/java/com/follow/bible/FileMain.java
@@ -0,0 +1,17 @@
+package com.follow.bible;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * Created by wangxin on 15/11/19.
+ */
+public class FileMain {
+
+    public static void main(String[] args) {
+        String path="";
+        BufferedReader reader = new BufferedReader(new FileReader(new File("")));
+
+    }
+}
